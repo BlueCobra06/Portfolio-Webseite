3d3b601d17839c9d090be3f8cac0eb3d873aad18
ddossner <dominik.dossner@tha.de>
2025-08-26 20:14:42 +0200

    About Page angefangen

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -2,10 +2,10 @@ const navbar = () => {
     return (
         <nav>
             <div className="flex justify-center p-3 gap-4 fixed w-full bg-gradient-to-r from-indigo-900/85 to-indigo-800/85 backdrop-blur-lg shadow-xl">
-                <span className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">Home</span>
-                <span className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">About</span>
-                <span className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">Projects</span>
-                <span className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">Contact</span>
+                <a href="#home" className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">Home</a>
+                <a href="#about" className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">About</a>
+                <a href="#projects" className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">Projects</a>
+                <a href="#contact" className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">Contact</a>
             </div>
         </nav>
     );
