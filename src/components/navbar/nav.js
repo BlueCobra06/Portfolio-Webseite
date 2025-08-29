cd3d8be2a44c99e8680c144f79a39fb20eabc6d2
ddossner <dominik.dossner@tha.de>
2025-08-29 15:17:10 +0200

    Problem: Navbar wird blurry unter Objekten gefixed

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -1,7 +1,7 @@
 const navbar = () => {
     return (
         <nav>
-            <div className="flex justify-center p-3 gap-4 fixed w-full bg-gradient-to-r from-indigo-900/85 to-indigo-800/85 backdrop-blur-lg shadow-xl">
+            <div className="flex justify-center p-3 gap-4 fixed w-full bg-gradient-to-r from-indigo-900/95 to-indigo-800/95 shadow-xl z-50">
                 <a href="#home" className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">Home</a>
                 <a href="#about" className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">About</a>
                 <a href="#projects" className="text-white font-bold text-xl cursor-pointer px-4 py-2 hover:shadow-lg hover:bg-white/10 transition-all duration-200 rounded-xl">Projects</a>
